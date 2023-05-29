facd04c63f1163d4e68572ff861dd5b247ccb4e9
Anubis211 <114920182+Franubi@users.noreply.github.com>
2023-05-29 13:26:27 +0200

    Create ejercicio2.py

---
+++ ejercicio2.py
@@ -0,0 +1,11 @@
+x = 3
+z = 3.3
+
+print("El tipo de x es ",type(x))
+print("El tipo de z es ",type(z))
+
+x = str(x)
+z = str(z)
+
+print("El tipo de x es ",type(x))
+print("El tipo de z es ",type(z))
